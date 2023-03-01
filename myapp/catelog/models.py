bbc8e09bee95231c22389276341b980b009c33f5
Ruveshan <ruveshannaik27@gmail.com>
2023-03-01 09:12:31 +0200

    third commit

--- myapp/catelog/models.py
+++ myapp/catelog/models.py
@@ -1,29 +1,26 @@
 from django.db import models
 
-# Create your models here.
-
 class album(models.Model):
     albumName = models.CharField(max_length=100,unique=True)
     albumArtist = models.CharField(max_length=100,unique=False)
     albumRealeaseYear = models.CharField(max_length=100,unique=False)
     albumCover = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    songs = models.ManyToManyField('song', related_name='albums')
+    awards = models.ManyToManyField('awards', related_name='albums')
 
     def __str__(self):
         return self.albumName
 
 class song(models.Model):
-    songName = models.CharField(max_length=100,unique=True)
+    songTitle = models.CharField(max_length=100,unique=True)
     songDuration = models.CharField(max_length=100,unique=False)
 
     def __str__(self):
-        return self.songName
+        return self.songTitle
 
 class awards(models.Model):
     awardName = models.CharField(max_length=100,unique=True)
     dateOfAward = models.CharField(max_length=100,unique=False)
-
+    
     def __str__(self):
         return self.awardName
-
-
-    
--- myapp/catelog/serializers.py
+++ myapp/catelog/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
 from .models import album, song, awards
 
-class albumSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = album
-        fields = ('__all__')
-
 class songSerializers(serializers.ModelSerializer):
     class Meta:
         model = song
-        fields = ('__all__')
+        fields = '__all__'
 
 class awardsSerializers(serializers.ModelSerializer):
     class Meta:
         model = awards
-        fields = ('__all__')
+        fields = '__all__'
+
+class albumSerializers(serializers.ModelSerializer):
+    song = songSerializers(many=True)
+    awards = awardsSerializers(many=True)
+
+    class Meta:
+        model = album
+        fields = '__all__'
